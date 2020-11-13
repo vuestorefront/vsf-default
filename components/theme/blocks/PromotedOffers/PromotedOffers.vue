82eeaa55f9962b9146c1fa00d80bd8ecd4b2dd7a
tkostuch <tkostuch@divante.pl>
2020-11-13 14:59:15 +0100

    remove lazy loading images

--- components/theme/blocks/PromotedOffers/PromotedOffers.vue
+++ components/theme/blocks/PromotedOffers/PromotedOffers.vue
@@ -9,7 +9,7 @@
         <router-link :to="localizedRoute(banner.link)">
           <div
             class="offer"
-            v-lazy:background-image="banner.image[supportsWebp ? 'webp' : 'fallback']"
+            :style="{backgroundImage: `url('${banner.image[supportsWebp ? 'webp' : 'fallback']}')`}"
           >
             <h2 class="title m0 h1">
               {{ banner.title }}
@@ -30,7 +30,7 @@
           <router-link :to="localizedRoute(banner.link)">
             <div
               class="offer offer-small border-box p5 flex bg-cl-th-accent"
-              v-lazy:background-image="banner.image[supportsWebp ? 'webp' : 'fallback']"
+              :style="{backgroundImage: `url('${banner.image[supportsWebp ? 'webp' : 'fallback']}')`}"
             >
               <h2 class="title m0 h1">
                 {{ banner.title }}
@@ -54,7 +54,7 @@
         <router-link :to="localizedRoute(banner.link)">
           <div
             class="offer offer-product border-box p5 flex bg-cl-th-accent"
-            v-lazy:background-image="banner.image[supportsWebp ? 'webp' : 'fallback']"
+            :style="{backgroundImage: `url('${banner.image[supportsWebp ? 'webp' : 'fallback']}')`}"
           >
             <h2 class="title m0 h1">
               {{ banner.title }}
@@ -153,10 +153,6 @@ export default {
   .offer-product {
     height: 330px;
     background-position: 50% 20%;
-
-    @media (max-width: 767px) {
-      height: 330px;
-    }
   }
   .title {
     @media (max-width: 767px) {
