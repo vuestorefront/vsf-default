a84921508605e492540023bcbeea2bf73a0e27ac
Fifciuu <filip.jdrasik@gmail.com>
2020-11-16 17:34:20 +0100

    refactor: responsize full width banner and HeadImage as a functional

--- components/core/blocks/MainSlider/HeadImage.vue
+++ components/core/blocks/MainSlider/HeadImage.vue
@@ -1,69 +1,35 @@
-<template>
+<template functional>
   <section class="head-image w-100 bg-cl-th-accent cl-white">
     <div
-      class="container w-100 h-100 cl-black"
-      :style="{backgroundImage: `url('${currentImage.image[supportsWebp ? 'webp' : 'fallback']}')`}"
-      v-if="currentImage"
+      class="head-image__container container w-100 h-100 cl-black"
+      :class="{'webp': props.supportsWebp}"
+      v-if="props.currentImage"
     >
       <div class="head-image-content">
         <h1 class="title" data-testid="mainSliderTitle">
-          {{ currentImage.title }}
+          {{ props.currentImage.title }}
         </h1>
         <p
           class="subtitle mb0 serif h3"
           data-testid="mainSliderSubtitle"
         >
-          {{ currentImage.subtitle }}
+          {{ props.currentImage.subtitle }}
         </p>
-        <!-- <div class="align-center inline-flex">
-          <button-outline :link="currentImage.link" color="light">
-            {{ currentImage.button_text }}
-          </button-outline>
-        </div> -->
       </div>
     </div>
   </section>
 </template>
 
 <script>
-import { currentStoreView } from '@vue-storefront/core/lib/multistore'
-import { mapGetters } from 'vuex'
-
-const image = {
-  webp: "/assets/full_width_banner.webp",
-  fallback: "/assets/full_width_banner.jpg"
-}
-
-const headImage = {
-  de: {
-      title: "Neue Wege beschreiten.",
-      subtitle: "Eine Mode kann sich zu einem neuen Stil durchsetzen und offenbart die neusten Kreationen von Designern, Technologen, Ingenieuren und Designmanagern.",
-      link: "/women/frauen-20",
-      image
-  },
-  it: {
-      title: "Cammina la passeggiata.",
-      subtitle: "Una moda può diventare lo stile prevalente nel comportamento o manifestare le ultime creazioni di designer, tecnologi, ingegneri e responsabili del design.",
-      link: "/women/la-donne-20",
-      image
-  },
-  default: {
-        title: "Walk the walk.",
-        subtitle: "A fashion can become the prevailing style in behaviour or manifest the newest creations of designers, technologists, engineers, and design managers.",
-        link: "/women.html",
-        image
-  }
-}
-
 export default {
-  computed: {
-    currentImage () {
-      const { storeCode } = currentStoreView()
-      
-      return headImage[storeCode] || headImage.default
+  props: {
+    currentImage: {
+      type: Object,
+      required: true
     },
-    supportsWebp () {
-      return this.$store.state.ui.supportsWebp
+    supportsWebp: {
+      type: Boolean,
+      required: true
     }
   }
 }
@@ -98,6 +64,86 @@ export default {
 
 .head-image {
   height: 640px;
+
+  &__container {
+    background-image: url('/assets/full_width_banner-800.jpg');
+    @media (min-width: 850px) {
+      background-image: url('/assets/full_width_banner-900.jpg');
+    }
+    @media (min-width: 950px) {
+      background-image: url('/assets/full_width_banner-1000.jpg');
+    }
+    @media (min-width: 1050px) {
+      background-image: url('/assets/full_width_banner-1100.jpg');
+    }
+    @media (min-width: 1150px) {
+      background-image: url('/assets/full_width_banner-1200.jpg');
+    }
+    @media (min-width: 1250px) {
+      background-image: url('/assets/full_width_banner-1300.jpg');
+    }
+    @media (min-width: 1350px) {
+      background-image: url('/assets/full_width_banner-1400.jpg');
+    }
+    @media (min-width: 1450px) {
+      background-image: url('/assets/full_width_banner-1500.jpg');
+    }
+    @media (min-width: 1550px) {
+      background-image: url('/assets/full_width_banner-1600.jpg');
+    }
+    @media (min-width: 1650px) {
+      background-image: url('/assets/full_width_banner-1700.jpg');
+    }
+    @media (min-width: 1750px) {
+      background-image: url('/assets/full_width_banner-1800.jpg');
+    }
+    @media (min-width: 1850px) {
+      background-image: url('/assets/full_width_banner-1900.jpg');
+    }
+    @media (min-width: 1950px) {
+      background-image: url('/assets/full_width_banner.jpg');
+    }
+
+    &.webp {
+      background-image: url('/assets/full_width_banner-800.webp');
+      @media (min-width: 850px) {
+        background-image: url('/assets/full_width_banner-900.webp');
+      }
+      @media (min-width: 950px) {
+        background-image: url('/assets/full_width_banner-1000.webp');
+      }
+      @media (min-width: 1050px) {
+        background-image: url('/assets/full_width_banner-1100.webp');
+      }
+      @media (min-width: 1150px) {
+        background-image: url('/assets/full_width_banner-1200.webp');
+      }
+      @media (min-width: 1250px) {
+        background-image: url('/assets/full_width_banner-1300.webp');
+      }
+      @media (min-width: 1350px) {
+        background-image: url('/assets/full_width_banner-1400.webp');
+      }
+      @media (min-width: 1450px) {
+        background-image: url('/assets/full_width_banner-1500.webp');
+      }
+      @media (min-width: 1550px) {
+        background-image: url('/assets/full_width_banner-1600.webp');
+      }
+      @media (min-width: 1650px) {
+        background-image: url('/assets/full_width_banner-1700.webp');
+      }
+      @media (min-width: 1750px) {
+        background-image: url('/assets/full_width_banner-1800.webp');
+      }
+      @media (min-width: 1850px) {
+        background-image: url('/assets/full_width_banner-1900.webp');
+      }
+      @media (min-width: 1950px) {
+        background-image: url('/assets/full_width_banner.webp');
+      }
+    }
+  }
 }
 .container {
   background-size: cover;
